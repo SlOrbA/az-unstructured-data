6347a5b9b9c18709846e9ce1698535eddc222a77
Lari Korpi <lari.korpi@tieto.com>
2021-06-10 13:49:56 +0300

    using explicit client and not Azure Functions framework

--- functions/EventHubGenerator/index.ts
+++ functions/EventHubGenerator/index.ts
@@ -1,4 +1,5 @@
 import { AzureFunction, Context } from "@azure/functions"
+const { EventHubProducerClient } = require("@azure/event-hubs");
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
     var timeStamp = new Date().toISOString();
@@ -6,6 +7,12 @@ const timerTrigger: AzureFunction = async function (context: Context, myTimer: a
     const sendingDisabled = process.env.DISABLED ? true : false;
     const topic = process.env.TOPIC ? process.env.TOPIC : 'Generator Message';
     const body = process.env.BODY ? process.env.BODY : 'Generator Message Body';
+
+    const connectionString = process.env.GENERATOR_EVENT_HUB_CONNECTION;
+    const eventHubName = process.env.ENDPOINT_EVENT_HUB_NAME;  
+
+    // Create a producer client to send messages to the event hub.
+    const producer = new EventHubProducerClient(connectionString, eventHubName);
 
     const message = {
         topic:topic,
@@ -26,7 +33,20 @@ const timerTrigger: AzureFunction = async function (context: Context, myTimer: a
         } else {
             context.log('Bindings are not ok!');
             context.log('context.bindings: ' + JSON.stringify(context.bindings));
+            
+            // Prepare a batch of three events.
+            const batch = await producer.createBatch();
+            batch.tryAdd(message);
+
+            // Send the batch to the event hub.
+            await producer.sendBatch(batch);
+
+            // Close the producer client.
+            await producer.close();
+
+            console.log("A batch of three events have been sent to the event hub");
         }
+
     }
     context.log('Timer trigger function ran!', timeStamp);
 };
--- functions/SecondEventHubTrigger/index.ts
+++ functions/SecondEventHubTrigger/index.ts
@@ -1,12 +1,37 @@
 import { AzureFunction, Context } from "@azure/functions"
+const { EventHubProducerClient } = require("@azure/event-hubs");
 
 const eventHubTrigger: AzureFunction = async function (context: Context, eventHubMessages: any[]): Promise<void> {
     context.log(`Eventhub trigger function called for message array ${eventHubMessages}`);
     
-    eventHubMessages.forEach((message, index) => {
+    const connectionString = process.env.SECOND_EVENT_HUB_CONNECTION;
+    const eventHubName = process.env.SECOND_EVENT_HUB_NAME;
+
+    // Create a producer client to send messages to the event hub.
+    const producer = new EventHubProducerClient(connectionString, eventHubName);
+    
+    eventHubMessages.forEach(async (message, index) => {
         context.log(`Processed message ${message}`);
 
-        context.bindings.outputEventHubMessage.push(message);
+        if (context.bindings.outputEventHubMessage) {
+            context.bindings.outputEventHubMessage.push(message);
+            context.log('Message sent!');
+        } else {
+            context.log('Bindings are not ok!');
+            context.log('context.bindings: ' + JSON.stringify(context.bindings));
+            
+            // Prepare a batch of three events.
+            const batch = await producer.createBatch();
+            batch.tryAdd(message);
+
+            // Send the batch to the event hub.
+            await producer.sendBatch(batch);
+
+            // Close the producer client.
+            await producer.close();
+
+            console.log("A batch of three events have been sent to the event hub");
+        }
     });
     context.done();
 };
